b5fc5b94d177993e0e5a5c77827e3c9d407321a4
aider <auder31101997@gmail.com>
2023-06-20 18:09:50 +0300

    added bio field

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -12,6 +12,7 @@
 
       <div>
         <p class="text-xl font-bold mb-2">{{ currentProfile.full_name }}</p>
+        <p class="text-lg font-semibold mt-2"> {{ currentProfile.bio }}</p>
 
         <div class="flex space-x-5">
           <div class="text-sm w-fit">
@@ -27,8 +28,6 @@
               {{ chartData.length > 1 ? `${chartData[chartData.length - 1][1]} sec` : 0 }}
             </p>
           </div>
-
-        <p class="text-lg font-semibold mt-2"> {{ currentProfile.bio }}</p>
         </div>
       </div>
     </div>
